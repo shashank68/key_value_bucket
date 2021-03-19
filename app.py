69f5bc97424797b5eb0131a2ad672907ac1687bf
shashank68 <shashankindiamanoj@gmail.com>
2021-03-19 20:44:11 +0530

    fix requirements

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 import os
+import psycopg2
+
 from flask import Flask, abort, request
-from peewee import IntegerField, CharField, DateTimeField, SqliteDatabase, Model
-from datetime import datetime, timezone
+from peewee import IntegerField, CharField, Model
+from datetime import datetime
 from playhouse.db_url import connect
 
 db = connect(os.environ.get("DATABASE_URL"))
